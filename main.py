fc1114d7f9d50c358450133c26aa262613795603
Doğa <dogagocmener@student.cag.edu.tr>
2022-12-11 16:14:23 +0100

    Update main.py

--- main.py
+++ main.py
@@ -12,67 +12,48 @@ root.geometry("1920x1080+0+0")
 root.config(bg="#2c3e50")
 root.state("zoomed")
 
-username = StringVar()
-password = StringVar()
-nickname = StringVar()
-gender = StringVar()
-email = StringVar()
-confirmationcode = StringVar()
-confirmationdate = StringVar()
-regdate = StringVar()
-linkdate = StringVar()
-date=dt.datetime.now()
+username, password, nickname, gender, email, confirmationcode, confirmationdate, regdate, linkdate, date = StringVar(), StringVar(), StringVar(), StringVar(), StringVar(), StringVar(), StringVar(), StringVar(), StringVar(), dt.datetime.now()
 format_date=f"{date:%a, %b %d %Y}"
 
-entries_frame = Frame(root, bg="#535c68")
+entries_frame, title = Frame(root, bg="#535c68"), Label(entries_frame, text="Player Management System", font=("Calibri", 18, "bold"), bg="#535c68", fg="white")
 entries_frame.pack(side=TOP, fill=X)
-title = Label(entries_frame, text="Player Management System", font=("Calibri", 18, "bold"), bg="#535c68", fg="white")
 title.grid(row=0, columnspan=2, padx=10, pady=20, sticky="w")
 
-lblUserName = Label(entries_frame, text="Username", font=("Calibri", 16), bg="#535c68", fg="white")
+lblUserName, txtUserName = Label(entries_frame, text="Username", font=("Calibri", 16), bg="#535c68", fg="white"), Entry(entries_frame, textvariable=username, font=("Calibri", 16), width=30)
 lblUserName.grid(row=1, column=0, padx=10, pady=10, sticky="w")
-txtUserName = Entry(entries_frame, textvariable=username, font=("Calibri", 16), width=30)
 txtUserName.grid(row=1, column=1, padx=10, pady=10, sticky="w")
 
-lblPassword = Label(entries_frame, text="Password", font=("Calibri", 16), bg="#535c68", fg="white")
+lblPassword, txtPassword = Label(entries_frame, text="Password", font=("Calibri", 16), bg="#535c68", fg="white"), Entry(entries_frame, textvariable=password, font=("Calibri", 16), width=30)
 lblPassword.grid(row=1, column=2, padx=10, pady=10, sticky="w")
-txtPassword = Entry(entries_frame, textvariable=password, font=("Calibri", 16), width=30)
 txtPassword.grid(row=1, column=3, padx=10, pady=10, sticky="w")
 
-lblNickname = Label(entries_frame, text="Nickname", font=("Calibri", 16), bg="#535c68", fg="white")
+lblNickname, txtNickname = Label(entries_frame, text="Nickname", font=("Calibri", 16), bg="#535c68", fg="white"), Entry(entries_frame, textvariable=nickname, font=("Calibri", 16), width=30)
 lblNickname.grid(row=2, column=0, padx=10, pady=10, sticky="w")
-txtNickname = Entry(entries_frame, textvariable=nickname, font=("Calibri", 16), width=30)
 txtNickname.grid(row=2, column=1, padx=10, pady=10, sticky="w")
 
-lblEmail = Label(entries_frame, text="Email", font=("Calibri", 16), bg="#535c68", fg="white")
+lblEmail, txtEmail = Label(entries_frame, text="Email", font=("Calibri", 16), bg="#535c68", fg="white"), Entry(entries_frame, textvariable=email, font=("Calibri", 16), width=30)
 lblEmail.grid(row=2, column=2, padx=10, pady=10, sticky="w")
-txtEmail = Entry(entries_frame, textvariable=email, font=("Calibri", 16), width=30)
 txtEmail.grid(row=2, column=3, padx=10, pady=10, sticky="w")
 
-lblGender = Label(entries_frame, text="Gender", font=("Calibri", 16), bg="#535c68", fg="white")
-lblGender.grid(row=3, column=0, padx=10, pady=10, sticky="w")
-comboGender = ttk.Combobox(entries_frame, font=("Calibri", 16), width=28, textvariable=gender, state="readonly")
+lblGender, comboGender = Label(entries_frame, text="Gender", font=("Calibri", 16), bg="#535c68", fg="white"), ttk.Combobox(entries_frame, font=("Calibri", 16), width=28, textvariable=gender, state="readonly")
 comboGender['values'] = ("Male", "Female")
+lblGender.grid(row=3, column=0, padx=10, pady=10, sticky="w")
 comboGender.grid(row=3, column=1, padx=10, sticky="w")
 
-lblConfirmationCode = Label(entries_frame, text="Confirmation Code", font=("Calibri", 16), bg="#535c68", fg="white")
+lblConfirmationCode, txtConfirmationCode = Label(entries_frame, text="Confirmation Code", font=("Calibri", 16), bg="#535c68", fg="white"), Entry(entries_frame, textvariable=confirmationcode, font=("Calibri", 16), width=30)
 lblConfirmationCode.grid(row=3, column=2, padx=10, pady=10, sticky="w")
-txtConfirmationCode = Entry(entries_frame, textvariable=confirmationcode, font=("Calibri", 16), width=30)
 txtConfirmationCode.grid(row=3, column=3, padx=10, sticky="w")
 
-lblConfirmationDate = Label(entries_frame, text="Confirmation Date", font=("Calibri", 16), bg="#535c68", fg="white")
+lblConfirmationDate, txtConfirmationDate = Label(entries_frame, text="Confirmation Date", font=("Calibri", 16), bg="#535c68", fg="white"), Entry(entries_frame, textvariable=format_date, font=("Calibri", 16), width=30)
 lblConfirmationDate.grid(row=4, column=0, padx=10, pady=10, sticky="w")
-txtConfirmationDate = Entry(entries_frame, textvariable=format_date, font=("Calibri", 16), width=30)
 txtConfirmationDate.grid(row=4, column=1, padx=10, sticky="w")
 
-lblRegDate = Label(entries_frame, text="Reg Date", font=("Calibri", 16), bg="#535c68", fg="white")
+lblRegDate, txtRegDate = Label(entries_frame, text="Reg Date", font=("Calibri", 16), bg="#535c68", fg="white"), Entry(entries_frame, textvariable=format_date, font=("Calibri", 16), width=30)
 lblRegDate.grid(row=4, column=2, padx=10, pady=10, sticky="w")
-txtRegDate = Entry(entries_frame, textvariable=format_date, font=("Calibri", 16), width=30)
 txtRegDate.grid(row=4, column=3, padx=10, sticky="w")
 
-lblLinkDate = Label(entries_frame, text="Link Date", font=("Calibri", 16), bg="#535c68", fg="white")
+lblLinkDate, txtLinkDate = Label(entries_frame, text="Link Date", font=("Calibri", 16), bg="#535c68", fg="white"), Entry(entries_frame, textvariable=format_date, font=("Calibri", 16), width=30)
 lblLinkDate.grid(row=5, column=0, padx=10, pady=10, sticky="w")
-txtLinkDate = Entry(entries_frame, textvariable=format_date, font=("Calibri", 16), width=30)
 txtLinkDate.grid(row=5, column=1, padx=10, sticky="w")
 
 
@@ -132,6 +113,7 @@ def clearAll():
 
 btn_frame = Frame(entries_frame, bg="#535c68")
 btn_frame.grid(row=6, column=0, columnspan=4, padx=10, pady=10, sticky="w")
+
 btnAdd = Button(btn_frame, command=add_player, text="Add Details", width=15, font=("Calibri", 16, "bold"), fg="white",
                 bg="#16a085", bd=0).grid(row=0, column=0)
 btnEdit = Button(btn_frame, command=update_player, text="Update Details", width=15, font=("Calibri", 16, "bold"),
